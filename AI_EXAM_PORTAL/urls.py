311382fa2ee5c269667733d816140471c7f20737
abhinusekharlal <abhinuyouramigo123@gmail.com>
2025-01-23 16:05:00 +0530

    new ui started few pages completed

--- AI_EXAM_PORTAL/urls.py
+++ AI_EXAM_PORTAL/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Users import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', include('classroom.urls', namespace='classroom')),
     path('verify-email/<str:token>/', views.verify_email, name='verify-email'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
